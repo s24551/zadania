21113e44174ceb87ea63da0d2bc55a1212c07b92
s24551 <92578013+s24551@users.noreply.github.com>
2021-11-12 12:11:55 +0100

    Update s03-password.cpp

--- s03/s03-password.cpp
+++ s03/s03-password.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 #include <string>
 
-int main()
+auto main(int argc, char* argv[]) -> int
 {
-std::string haslo, drugie;
-
-    std::cout<<"Podaj dane: ";
-    std::cin>>drugie;
+std::string haslo;
+a = argv[1];
+if(haslo==a)
+{
+    std::cout<<"OK!";
+}
     do{
     std::cout << "Podaj haslo: ";
     std::cin>>haslo;
-    }while(haslo!=drugie);
-
+    }while(haslo!=a);
     return 0;
 }
 
