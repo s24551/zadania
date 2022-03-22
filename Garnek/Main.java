24736fdedeb2940246f3a121a6efda74f3d52d40
s24551 <92578013+s24551@users.noreply.github.com>
2022-03-22 13:15:26 +0100

    Update Main.java

--- Garnek/Main.java
+++ Garnek/Main.java
@@ -1,12 +1,24 @@
 package com.company;
 
+import java.sql.SQLOutput;
+
 public class Main {
 
     public static void main(String[] args) {
-        Garnek garnek = new Garnek(5, 10, "Srebrny");//przypisanie wartosci do konstruktora argumentowego
+        Garnek garnek = new Garnek(5, 10, "Srebrny");   //przypisanie wartosci do konstruktora argumentowego
 
         System.out.println(garnek.kolor);
         System.out.println(garnek.srednica);
         System.out.println(garnek.wysokosc);
+
+
+        String wiadomosc = garnek.gotuj();  //przypisanie zwrotu z metody gotuj w klasie garnek do stringa wiadomosc
+        System.out.println(wiadomosc);
+
+
+        String czySol = garnek.gotuj(false); //przypisanie wartosci do metody czySol w klasie Garnek
+        System.out.println(czySol);
+
+        garnek.bezInfo(); //wywolanie void metod
     }
 }
