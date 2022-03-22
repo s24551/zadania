354292a85a2a1adb07e50cd4bc981efd73baa7b6
s24551 <92578013+s24551@users.noreply.github.com>
2022-03-22 13:00:12 +0100

    Create Garnek.java

---
+++ Garnek/Garnek.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Garnek {
+    int wysokosc;  //pola o nazwie wysokosc
+    int srednica;
+    String kolor;
+
+    public Garnek(){        //konstruktor bezargumentowy
+        
+    }
+
+    public Garnek(int srednica,int wysokosc, String kolor){
+        this.srednica=srednica;
+        this.wysokosc=wysokosc;
+        this.kolor=kolor;
+    }
+
+}
+
