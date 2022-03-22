10f3f49e83c393861e89efb72d4d37a562fde28f
s24551 <92578013+s24551@users.noreply.github.com>
2022-03-22 13:15:43 +0100

    Update Garnek.java

--- Garnek/Garnek.java
+++ Garnek/Garnek.java
@@ -6,14 +6,41 @@ public class Garnek {
     String kolor;
 
     public Garnek(){        //konstruktor bezargumentowy
-        
+
     }
 
-    public Garnek(int srednica,int wysokosc, String kolor){
+    public Garnek(int srednica,int wysokosc, String kolor){   //konstruktor argumentowy
         this.srednica=srednica;
         this.wysokosc=wysokosc;
         this.kolor=kolor;
     }
 
+    public String gotuj(){ //stworzenie metody
+    return "Gotowanie w trakcie";   //instrukcja metody
+    }
+
+    public String gotuj(boolean czySol)  //stworzenie metody przyjmujacej argument
+    {
+        if(czySol){
+            return "Gotownaie z Sola";
+        }else{
+            return "Gotowanie bez soli";
+        }
+    }
+    public void bezInfo(){
+        wlaczGarnek();
+        gotojPrzezPolGodziny();
+        wylaczGarnek();
+    }
+public void wlaczGarnek(){  //stworzenie metody ktora nie zwraca zadnej informacji
+    System.out.println("Wlacz garnek");
+}
+public void gotojPrzezPolGodziny(){
+    System.out.println("Gotowanie przez 30 minut");
+}
+
+public void wylaczGarnek(){
+    System.out.println("Wylaczanie garnka");
+}
 }
 
