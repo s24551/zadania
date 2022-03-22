50ae9af44a227a3b81f07d5443b97bee8d8ae036
s24551 <92578013+s24551@users.noreply.github.com>
2022-03-22 01:11:28 +0100

    Create Main_circle.java

---
+++ Main_circle.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) {
+        MyCircle myCir = new MyCircle(10);
+        System.out.println(myCir.Obw());
+        System.out.println(myCir.Pol());
+        System.out.println(myCir.D());
+    }
+}
+
